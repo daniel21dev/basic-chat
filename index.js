2412fb021e52a51a86bf7a37ca62253fdfe4641a
daniel <daniel21dev@gmail.com>
2021-10-25 10:13:49 -0500

    agrega envio de archivos y mejora el diseño

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + 'index.html');
-// });
-
 app.use(express.static( path.join(__dirname,'public')));
 
 io.on('connection', (socket) => {
@@ -22,8 +18,16 @@ io.on('connection', (socket) => {
         io.emit('escribiendo',e)
     })
 
-    socket.on('image',(img)=>{
-        io.emit('image',img)
+    socket.on('image',({img, type})=>{
+        if( type.includes('image') ){
+            return io.emit('image',img)
+        }else{
+            const url = '/files/' + Date.now() + '.' + type.split('/')[1];
+            require("fs").writeFile( path.join(__dirname,'/public/'+url), img, 'base64', function(err) {
+                if(!err)
+                    io.emit('file', 'http://localhost:3000' + url ) 
+            });
+        }
     })
 });
 
